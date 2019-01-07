82787467569ace89dec5f1e88b8d5ebd01b82b90
alex-samari <alexsamarim@gmail.com>
2019-01-07 10:37:32 -0500

    Converting the Ninja component from a class component to a functional(stateless) component

--- src/Ninjas.js
+++ src/Ninjas.js
@@ -1,31 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Ninjas extends Component {
-    render() {
-        //  console.log(this.props);
-        //  destructuring properties
-        const { ninjas } = this.props;
-        //  alternative method
-        //  const ninjas = this.props.ninjas;
-        //  mapping allows us to duplicate an array
-        const ninjaList = ninjas.map(ninja => {
-            return (
-                //  we need to define the key for the surrounding element so that react knows which element to update/delete
-                //  later on when we access differnt elements in the ninjas array
-                <div className='ninja' key={ninja.id}>
-                    <div>Name: {ninja.name}</div>
-                    <div>Age: {ninja.age}</div>
-                    <div>Belt: {ninja.belt}</div>
-                    <br></br>
-                </div>
-            )
-        })
+//  passing the 'ninjas' props from App.js and assigning it to 'Ninjas' const
+const Ninjas = ({ninjas}) => {
+
+    //  mapping allows us to duplicate an array
+    const ninjaList = ninjas.map(ninja => {
         return (
-            <div className="ninja-list">
-                {ninjaList}
+            //  we need to define the key for the surrounding element so that react knows which element to update/delete
+            //  later on when we access differnt elements in the ninjas array
+            <div className='ninja' key={ninja.id}>
+                <div>Name: {ninja.name}</div>
+                <div>Age: {ninja.age}</div>
+                <div>Belt: {ninja.belt}</div>
+                <br></br>
             </div>
         )
-    }
+    })
+    return (
+        <div className="ninja-list">
+            {ninjaList}
+        </div>
+    )
 }
 
 export default Ninjas;
