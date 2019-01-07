2e1a5a77a16adaaee7d6c8edddaeb29236f1e23a
alex-samari <alexsamarim@gmail.com>
2019-01-07 10:17:51 -0500

    Added "Ninjas.js" a stateless component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
+import Ninjas from './Ninjas';
 
 class App extends Component {
+  state = {
+    ninjas: [
+      { name: 'Alex', age: '21', belt: 'Black', id: 1 },
+      { name: 'John', age: '22', belt: 'Green', id: 2 },
+      { name: 'Jane', age: '24', belt: 'Orange', id: 3 }
+    ]
+  }
   render() {
     return (
       <div className="App">
         <h1>My first React app!</h1>
         <p>Welcome!</p>
+        <Ninjas ninjas={this.state.ninjas} />
+        {/* <Ninjas name="John" age="20" belt="Orange"/> */}
+
       </div>
     );
   }
--- src/Ninjas.js
+++ src/Ninjas.js
@@ -1,12 +1,27 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
-class Ninjas extends Component{
-    render(){
-        return(
-            <div className='ninja'>
-                <div>Name: Alex</div>
-                <div>Age: 21</div>
-                <div>Belt: Black</div>
+class Ninjas extends Component {
+    render() {
+        //  console.log(this.props);
+        //  destructuring properties
+        const { ninjas } = this.props;
+        //  alternative method
+        //  const ninjas = this.props.ninjas;
+        const ninjaList = ninjas.map(ninja => {
+            return (
+                //  we need to define the key for the surrounding element so that react knows which element to update/delete
+                //  later on when we access differnt elements in the ninjas array
+                <div className='ninja' key={ninja.id}>
+                    <div>Name: {ninja.name}</div>
+                    <div>Age: {ninja.age}</div>
+                    <div>Belt: {ninja.belt}</div>
+                    <br></br>
+                </div>
+            )
+        })
+        return (
+            <div className="ninja-list">
+                {ninjaList}
             </div>
         )
     }
