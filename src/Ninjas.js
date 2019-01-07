18917681cca5a6aeca8e2517d7941bd93b7588e8
alex-samari <alexsamarim@gmail.com>
2019-01-07 10:43:06 -0500

    Added a conditional output to Ninjas.js

--- src/Ninjas.js
+++ src/Ninjas.js
@@ -5,17 +5,17 @@ const Ninjas = ({ninjas}) => {
 
     //  mapping allows us to duplicate an array
     const ninjaList = ninjas.map(ninja => {
-        return (
+        return ninja.age > 22 ? (
             //  we need to define the key for the surrounding element so that react knows which element to update/delete
-            //  later on when we access differnt elements in the ninjas array
-            <div className='ninja' key={ninja.id}>
-                <div>Name: {ninja.name}</div>
-                <div>Age: {ninja.age}</div>
-                <div>Belt: {ninja.belt}</div>
-                <br></br>
-            </div>
-        )
-    })
+                //  later on when we access differnt elements in the ninjas array
+                <div className='ninja' key={ninja.id}>
+                    <div>Name: {ninja.name}</div>
+                    <div>Age: {ninja.age}</div>
+                    <div>Belt: {ninja.belt}</div>
+                    <br></br>
+                </div>
+        ) : null;
+    });
     return (
         <div className="ninja-list">
             {ninjaList}
